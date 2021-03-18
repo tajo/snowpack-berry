56cdcad57d4dc6d2cb70db3dd02d3216454e32f9
Vojtech Miksu <vojtech@uber.com>
2021-03-18 13:47:02 -0700

    init commit

---
+++ one/run.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+console.log("huh");
+
+// const { startServer, createConfiguration } = require("snowpack");
+// const path = require("path");
+// const config = createConfiguration({
+//   //root: "????",
+//   mount: {
+//     [path.join(process.cwd(), "./src/")]: "/",
+//     [path.join(__dirname, "./src/")]: "/",
+//   },
+// });
+// startServer({ config });
